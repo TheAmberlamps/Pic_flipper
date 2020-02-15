3795c2bfeb00ae4020408766f243b78d2b9a2721
Paul Unsted <ed__84@hotmail.com>
2020-02-14 19:24:49 -0500

    it's been a slog, but i finally got the buttons to perform like i want them to. taking a break now, next commit should be close to complete.

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,69 @@
 document.addEventListener(
   "click",
   function(event) {
-    if (!event.target.matches(".pic")) return;
-    if (event.target.matches("#tl")) {
-      var getTleft = document.getElementById("tl");
-      getTleft.style.transform = "scaleX(-1)";
-      console.log("getTleft= " + getTleft);
+    var tLeft = document.getElementById("tl");
+    var tRight = document.getElementById("tr");
+    var bLeft = document.getElementById("bl");
+    var bRight = document.getElementById("br");
+    if (!event.target.matches(".button")) return;
+    if (event.target.matches("#tlDirU")) {
+      tLeft.style.transform = "scaleX(-1)";
+      console.log("tLeft= " + tLeft);
+      console.log(tLeft.style.transform.valueOf);
     }
     if (event.target.matches("#tr")) {
-      var getTright = document.getElementById("tr");
-      console.log("getTright= " + getTright);
+      console.log("tRight= " + tRight);
     }
     if (event.target.matches("#bl")) {
-      var getBleft = document.getElementById("bl");
-      getBleft.style.transform = "scaleY(-1) scaleX(-1)";
-      console.log("getBleft= " + getBleft);
+      bLeft.style.transform = "scaleY(-1) scaleX(-1)";
+      console.log("bLeft= " + bLeft);
     }
     if (event.target.matches("#br")) {
-      var getBright = document.getElementById("br");
-      getBright.style.transform = "scaleY(-1)";
-      console.log("getBright= " + getBright);
+      bRight.style.transform = "scaleY(-1)";
+      console.log("bRight= " + bRight);
     }
   },
   false
 );
+
+/* document.addEventListener(
+  "click",
+  function(event) {
+    var tLeft = document.getElementById("tl");
+    if (!event.target.matches(".button")) return;
+    event.preventDefault();
+    if (event.target.matches("#tlDirU")) {
+      console.log("bap");
+      if (tLeft.style.transform === "scaleX(1) scaleY(1)") {
+        tLeft.style.transform = "scaleY(-1)";
+        console.log("tLeft= " + tLeft);
+      } else {
+        tLeft.style.transform = "scaleY(1)";
+        console.log("tLeft= " + tLeft);
+      }
+    } else if (event.target.matches("#tlDirL")) {
+      if (tLeft.style.transform === "scaleX(1)") {
+        tLeft.style.transform = "scaleX(-1)";
+      } else {
+        tLeft.style.transform = "scaleX(1)";
+        console.log("tLeft= " + tLeft);
+      }
+    } else if (event.target.matches("#tlDirR")) {
+      if (tLeft.style.transform === "scaleX(1)") {
+        tLeft.style.transform = "scaleX(-1)";
+      } else {
+        tLeft.style.transform = "scaleX(1)";
+        console.log("tLeft= " + tLeft);
+      }
+    } else {
+      if (tLeft.style.transform === "scaleY(1)") {
+        tLeft.style.transform = "scaleY(-1)";
+      } else {
+        tLeft.style.transform = "scaleY(1)";
+        console.log("tLeft= " + tLeft);
+      }
+    }
+  },
+  false
+);
+*/
