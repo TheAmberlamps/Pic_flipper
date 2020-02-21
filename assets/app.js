44f44feabb07bb935b57f6b7442408ecaef14ace
Paul Unsted <ed__84@hotmail.com>
2020-02-20 23:53:50 -0500

    removed the console logs i was using to debug

--- assets/app.js
+++ assets/app.js
@@ -30,31 +30,21 @@ document.addEventListener(
     var bRight = document.getElementById("br");
     if (!event.target.matches(".button")) return;
     if (event.target.matches("#tlDirV")) {
-      console.log(tLeft.style);
-      console.log(tLeft.style.transform);
       flipV(tLeft);
-      console.log(tLeft.style.transform);
     } else if (event.target.matches("#tlDirH")) {
       flipH(tLeft);
-      console.log(tLeft.style.transform);
     } else if (event.target.matches("#blDirV")) {
       flipV(bLeft);
-      console.log(bLeft.style.transform);
     } else if (event.target.matches("#blDirH")) {
       flipH(bLeft);
-      console.log(bLeft.style.transform);
     } else if (event.target.matches("#trDirV")) {
       flipV(tRight);
-      console.log(tRight.style.transform);
     } else if (event.target.matches("#trDirH")) {
       flipH(tRight);
-      console.log(tRight.style.transform);
     } else if (event.target.matches("#brDirV")) {
       flipV(bRight);
-      console.log(bRight.style.transform);
     } else {
       flipH(bRight);
-      console.log(bRight.style.transform);
     }
   },
   false
